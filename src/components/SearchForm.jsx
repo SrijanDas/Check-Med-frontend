c94aaed7bcf4243c8615a93a26a8dd356d3b76bb
Srijan Das <srijandas18.sd@gmail.com>
2021-06-19 16:52:34 +0530

    working on issue #2 added three search buttons. Next TODO: render input field based on button selected

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,23 +1,41 @@
-import React from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Form from "react-bootstrap/Form";
 
 function SearchForm() {
+  const [btnSelect, setBtnSelect] = useState("1");
+
+  const buttons = [
+    { name: "Search by PIN", value: "1" },
+    { name: "Search by District", value: "2" },
+    { name: "Search by Map", value: "3" },
+  ];
+
   return (
     <>
       <h3 className="mt-4">Search for a medicine...</h3>
       <Form>
-        <Row>
-          <Col>
-            <Form.Control size="lg" type="text" />
-          </Col>
-          <Col>
-            <Button>Search</Button>
-          </Col>
-        </Row>
+        <Form.Control type="text" placeholder="Enter medicine name" />
+
+        <ButtonGroup className="mt-3">
+          {buttons.map((btn, idx) => (
+            <Button
+              key={idx}
+              variant={btnSelect === btn.value ? "primary" : "outline-primary"}
+              onClick={() => {
+                setBtnSelect(btn.value);
+              }}
+            >
+              {btn.name}
+            </Button>
+          ))}
+        </ButtonGroup>
+        <Button size="lg" variant="primary" className="mb-2">
+          Search
+        </Button>
       </Form>
+      {btnSelect}
     </>
   );
 }
