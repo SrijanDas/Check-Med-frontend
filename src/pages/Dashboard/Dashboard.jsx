76433d0c7972521402e60c52b56a12e315947cf4
Srijan Das <srijandas18.sd@gmail.com>
2021-06-20 12:33:23 +0530

    modified dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,41 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dashboard.css";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import LocalMallIcon from "@material-ui/icons/LocalMall";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import ReportIcon from "@material-ui/icons/Report";
 
 function Dashboard() {
+  const [active, setActive] = useState(1);
   return (
-    <div className="dashboard my-4">
-      <div className="mb-5">
+    <div className="dashboard">
+      <div className="dashboard__shopDetails">
         <h3 className="dashboard__title">Shop Name</h3>
         <p>Contact No: 1234567</p>
         <p>Address: NG Road, State, District, PIN-123</p>
       </div>
-      <div>
+
+      <div className="dashboard__tools">
         <div className="dashboard__cardsContainer">
-          {["Inventory", "Sales", "Reports"].map((item, indx) => (
-            <Link className="applink">
-              <Card
-                key={indx}
-                bg={indx === 0 ? "primary" : indx === 1 ? "success" : "danger"}
-                text="light"
-                className="dashboard__card shadow p-3 mb-5 rounded text-center"
-              >
-                <Card.Body>
-                  <h4 className="dashboard__cardsName">
-                    {indx === 0 ? (
-                      <i class="fas fa-box-open"></i>
-                    ) : indx === 1 ? (
-                      <i class="fas fa-clipboard-list"></i>
-                    ) : (
-                      <i class="fas fa-exclamation-circle"></i>
-                    )}{" "}
-                    {item}
-                  </h4>
-                </Card.Body>
-              </Card>
-            </Link>
-          ))}
+          <div
+            onClick={() => {
+              setActive(0);
+            }}
+            className={`dashboard__card inventory ${
+              active === 0 ? "active" : ""
+            }`}
+          >
+            <span className="dashboard__cardTitle">
+              <LocalMallIcon style={{ marginRight: "0.2rem" }} />
+              Inventory
+            </span>
+            <div className="dashboard__cardInfo">
+              <h1>20k</h1>
+            </div>
+            <span className="dashboard__cardFooter">
+              Last Updated: 2 days ago
+            </span>
+          </div>
+
+          <div
+            onClick={() => {
+              setActive(1);
+            }}
+            className={`dashboard__card sales ${active === 1 ? "active" : ""}`}
+          >
+            <span className="dashboard__cardTitle">
+              <MonetizationOnIcon style={{ marginRight: "0.2rem" }} />
+              Sales
+            </span>
+            <div className="dashboard__cardInfo">
+              <h1>12k</h1>{" "}
+              <p>
+                2.5 <ArrowDownwardIcon />
+              </p>
+            </div>
+            <span className="dashboard__cardFooter">
+              Compared to last month
+            </span>
+          </div>
+
+          <div
+            onClick={() => {
+              setActive(2);
+            }}
+            className={`dashboard__card reports ${
+              active === 2 ? "active" : ""
+            }`}
+          >
+            <span className="dashboard__cardTitle">
+              <ReportIcon style={{ marginRight: "0.2rem" }} />
+              Reports
+            </span>
+            <div className="dashboard__cardInfo">
+              <h1>8</h1>
+            </div>
+            <span className="dashboard__cardFooter">6 resolved</span>
+          </div>
         </div>
       </div>
     </div>
