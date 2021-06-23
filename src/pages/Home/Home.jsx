da18fccf2d6f16ea65367fa7a45f6abe19d3c3fe
Srijan Das <srijandas18.sd@gmail.com>
2021-06-23 13:34:00 +0530

    added search by pincode functionality

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,72 +5,163 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import axios from "../../helpers/axios";
-import { Button, Form } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import SentimentDissatisfiedIcon from "@material-ui/icons/SentimentDissatisfied";
 
 function Home() {
   const [medicineName, setMedicineName] = useState("");
   const [query, setQuery] = useState("");
+  const [error, setError] = useState(false);
 
-  const [cards, setCards] = useState([]);
   const [showHeader, setShowHeader] = useState(false);
+  const [cards, setCards] = useState([]);
+
+  const [btnSelect, setBtnSelect] = useState("1");
+
+  const buttons = [
+    { name: "Search by PIN", value: "1" },
+    { name: "Search by District", value: "2" },
+    { name: "Search by Map", value: "3" },
+  ];
+
+  const clearStates = () => {
+    setError(false);
+    setCards([]);
+    setShowHeader(false);
+  };
 
   const handleSearch = async (e) => {
     e.preventDefault();
     const formData = {
+      queryType: btnSelect,
       query: query.toLowerCase(),
       medicine_name: medicineName.toLowerCase(),
     };
     try {
       const response = await axios.post("/search", formData);
+      console.log(response.data);
       setShowHeader(true);
       setCards(response.data);
+      setError(false);
     } catch (error) {
-      console.log(error);
+      setCards([]);
+      setShowHeader(false);
+      setError(true);
     }
   };
 
   return (
     <div className="homePage">
       <Container fluid="sm">
-        <h2 className="searchForm__heading">Search for a medicine...</h2>
-        <Form className="mt-3" onSubmit={handleSearch}>
-          <Form.Control
-            type="text"
-            className="mb-3"
-            placeholder="Enter medicine name"
-            required
-            onChangeCapture={(e) => {
-              setMedicineName(e.target.value);
-            }}
-          />
-          <Form.Control
-            type="number"
-            className="pinInput mb-2"
-            placeholder="Enter PIN"
-            required
-            onChangeCapture={(e) => {
-              setQuery(e.target.value);
-            }}
-          />
-          <Button type="submit" variant="primary" className="btn-block">
-            Search
-          </Button>
-        </Form>
+        <Row>
+          <Col md={{ span: 10, offset: 1 }}>
+            <h2 className="searchForm__heading">Search for a medicine...</h2>
 
-        <h4 style={{ fontWeight: 400 }} className="mt-5">
-          {showHeader
-            ? cards.length
-              ? ` ${medicineName.toUpperCase()} is available here..`
-              : `${medicineName.toUpperCase()} is not available at your location`
-            : ""}
-        </h4>
+            {/* search method button */}
+            <ButtonGroup className="mt-3 mb-0">
+              {buttons.map((btn, idx) => (
+                <Button
+                  key={idx}
+                  className="mb-3"
+                  variant={
+                    btnSelect === btn.value ? "primary" : "outline-secondary"
+                  }
+                  onClick={() => {
+                    setBtnSelect(btn.value);
+                    clearStates();
+                  }}
+                >
+                  {btn.name}
+                </Button>
+              ))}
+            </ButtonGroup>
 
-        <Row xs={1} md={3} className="g-4">
-          {cards.map((cardData, indx) => (
-            <Col key={indx}>
-              <ShopCard cardData={cardData} />
-            </Col>
-          ))}
+            {/* Search form */}
+            {btnSelect === "3" ? (
+              <h3>Coming Soon...</h3>
+            ) : (
+              <Form className="" onSubmit={handleSearch}>
+                <Form.Control
+                  type="text"
+                  className="mb-3"
+                  placeholder="Enter medicine name"
+                  required
+                  onChangeCapture={(e) => {
+                    setMedicineName(e.target.value);
+                  }}
+                />
+                {btnSelect === "2" ? (
+                  <Form.Control
+                    type="text"
+                    placeholder="Select District"
+                    required
+                  />
+                ) : (
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter PIN"
+                    required
+                    onChangeCapture={(e) => {
+                      setQuery(e.target.value);
+                    }}
+                  />
+                )}
+
+                <Button
+                  type="submit"
+                  variant="primary"
+                  className="btn-block mt-2"
+                >
+                  Search
+                </Button>
+              </Form>
+            )}
+
+            {/* cards */}
+            {showHeader ? (
+              cards.length ? (
+                <h4 style={{ fontWeight: 400 }} className="mt-5">
+                  Medicine is available here...
+                </h4>
+              ) : (
+                <Alert variant="danger" className="my-4">
+                  This medicine is not available at your location{" "}
+                  <SentimentDissatisfiedIcon />
+                </Alert>
+              )
+            ) : (
+              ""
+            )}
+
+            <Row xs={1} md={3} className="g-4">
+              {cards.map((cardData, indx) => (
+                <Col key={indx}>
+                  <ShopCard cardData={cardData} />
+                </Col>
+              ))}
+            </Row>
+
+            {/* if errors */}
+            {error ? (
+              <Alert
+                variant="danger"
+                onClose={() => setError(false)}
+                dismissible
+                className="my-4"
+              >
+                <Alert.Heading>
+                  Something went wrong!{" "}
+                  <SentimentDissatisfiedIcon fontSize="large" />
+                </Alert.Heading>
+                <p>Check the medicine name.</p>
+              </Alert>
+            ) : (
+              ""
+            )}
+          </Col>
         </Row>
       </Container>
     </div>
