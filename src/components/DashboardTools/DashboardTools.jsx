6a403c1f0cf8a32b125a3964825da410ac2dfd71
Srijan Das <srijandas18.sd@gmail.com>
2021-07-04 17:43:01 +0530

    added dashboard sidebar

--- src/components/DashboardTools/DashboardTools.jsx
+++ src/components/DashboardTools/DashboardTools.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import "./DashboardTools.css";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import LocalMallIcon from "@material-ui/icons/LocalMall";
+import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import ReportIcon from "@material-ui/icons/Report";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function DashboardTools() {
+  const [active, setActive] = useState("Dashboard");
+
+  const optionsList = [
+    { icon: <DashboardIcon />, text: "Dashboard" },
+    { icon: <LocalMallIcon />, text: "Inventory" },
+    { icon: <MonetizationOnIcon />, text: "Sales" },
+    { icon: <ReportIcon />, text: "Reports" },
+    { icon: <NotificationsIcon />, text: "Notifications" },
+  ];
   return (
     <div>
-      <div class="dashboardTools">
-        <ul class="dashboardToolsList">
-          <li id="userProfile" class="sidebarList__item active">
-            <span class="sidebarList__itemText">Dashboard</span>
-          </li>
-          <li id="userProfile" class="sidebarList__item active">
-            <span class="sidebarList__itemText">Inventory</span>
-          </li>
-          <li id="userProfile" class="sidebarList__item active">
-            <span class="sidebarList__itemText">Sales</span>
-          </li>
-          <li id="userProfile" class="sidebarList__item active">
-            <span class="sidebarList__itemText">Reports</span>
-          </li>
+      <div className="dashboardTools">
+        <ul className="dashboardToolsList">
+          {optionsList.map((option, indx) => (
+            <li
+              onClick={() => {
+                setActive(option.text);
+              }}
+              className={`dashboardToolsList__item ${
+                active === option.text ? "active" : ""
+              }`}
+            >
+              <span className="dashboardToolsList__itemIcon">
+                {option.icon}
+              </span>
+              <span className="dashboardToolsList__itemText">
+                {option.text}
+              </span>
+              <span class="dashboardToolsList__itemTooltiptext">
+                {" "}
+                {option.text}
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./Dashboard.css";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Inventory from "../../components/Inventory/Inventory";
 import DashboardCards from "../../components/DashboardCards/DashboardCards";
@@ -10,27 +9,18 @@ function Dashboard() {
   const [active, setActive] = useState();
 
   return (
-    <>
-      <Container maxWidth="lg">
-        <div className="dashboard">
-          <DashboardTools className="dashboard__tools" />
-          <div className="dashboard__content">
-            <Typography
-              className="my-4"
-              align="center"
-              gutterBottom
-              variant="h5"
-            >
-              Dashboard
-            </Typography>
-            <DashboardCards active={active} setActive={setActive} />
-            <div className="dashboard__contentItem">
-              {active === 0 ? <Inventory /> : ""}
-            </div>
-          </div>
+    <div className="dashboard">
+      <DashboardTools className="dashboard__tools" />
+      <div className="dashboard__content">
+        <Typography className="my-4" align="center" gutterBottom variant="h5">
+          Dashboard
+        </Typography>
+        <DashboardCards active={active} setActive={setActive} />
+        <div className="dashboard__contentItem">
+          {active === 0 ? <Inventory /> : ""}
         </div>
-      </Container>
-    </>
+      </div>
+    </div>
   );
 }
 
