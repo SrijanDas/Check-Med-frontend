a239230d5aa4bb40b56ab142f1a8de1f6caf8518
Srijan Das <srijandas18.sd@gmail.com>
2021-07-21 21:19:17 +0530

    added feedback and contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,44 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container, TextField, Typography, Button } from "@material-ui/core";
 import React from "react";
+import "./Contact.css";
 
 function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <Container maxWidth="md">
+    <Container className="contactPage" maxWidth="md">
+      <Typography variant="h4">Contact Us</Typography>
       <Typography variant="h6" gutterBottom>
-        Contact Us
+        Email: checkmeds2021@gmail.com
       </Typography>
+
+      <form className="contactPage__form" onSubmit={handleSubmit}>
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Your Email"
+          variant="outlined"
+          required
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Message"
+          variant="outlined"
+          multiline={true}
+          required
+        />
+
+        <Button
+          style={{ marginTop: "1rem" }}
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,10 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Container,
+  TextField,
+  Typography,
+  Button,
+  MenuItem,
+} from "@material-ui/core";
 
 function Feedback() {
+  const [optionType, setOptionType] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    setOptionType(event.target.value);
+  };
+
+  const optionTypes = [
+    {
+      value: "Bug",
+      label: "Bug",
+    },
+    {
+      value: "Enhancement",
+      label: "Enhancement",
+    },
+    {
+      value: "Other",
+      label: "Other",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Feedback</h1>
-    </div>
+    <Container className="contactPage" maxWidth="md">
+      <Typography variant="h4">Feedback</Typography>
+      <Typography variant="h6" color="textSecondary" gutterBottom>
+        Give us your feedback to improve this website
+      </Typography>
+
+      <form className="contactPage__form" onSubmit={handleSubmit}>
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Your Email"
+          variant="outlined"
+          required
+        />
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Type"
+          variant="outlined"
+          required
+          select
+          value={optionType}
+          onChange={handleChange}
+        >
+          {optionTypes.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          margin="normal"
+          fullWidth
+          label="Feedback"
+          variant="outlined"
+          multiline={true}
+          required
+        />
+
+        <Button
+          style={{ marginTop: "1rem" }}
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
   );
 }
 
